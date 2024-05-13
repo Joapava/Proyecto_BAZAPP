7122ba6b8a34eb3e0e3329834456ab1208246416
Gabot22 <106275142+GaelBote@users.noreply.github.com>
2024-05-13 15:33:19 -0700

    Se modifico el mapa de ubicación
    -Se agrego la numeración de los espacios
    -Se agrego el cambio de color al seleccionar un espacio

--- prueba/lib/components/generar_botones.dart
+++ prueba/lib/components/generar_botones.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:prueba/pages/pagina_puestos%20copy.dart';
 
 // Define una enumeración para la dirección de los botones.
 enum ButtonDirection { horizontal, vertical }
+
+// Variable global para mantener el contador.
+int globalCounter = 1;
+
+class ButtonWithColorChange extends StatefulWidget {
+  final String label;
+
+  const ButtonWithColorChange({Key? key, required this.label})
+      : super(key: key);
+
+  @override
+  _ButtonWithColorChangeState createState() => _ButtonWithColorChangeState();
+}
+
+class _ButtonWithColorChangeState extends State<ButtonWithColorChange> {
+  bool isPressed = false;
+
+  void _toggleColor() {
+    setState(() {
+      isPressed = !isPressed;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 25,
+      width: 25,
+      margin: const EdgeInsets.all(2),
+      child: ElevatedButton(
+        onPressed: _toggleColor,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isPressed
+              ? const Color.fromARGB(184, 255, 11, 11)
+              : const Color.fromARGB(185, 9, 152, 255),
+          padding: EdgeInsets.zero, // Reduce el padding a cero
+          shape: RoundedRectangleBorder(
+            // Asegura que el botón sea cuadrado
+            borderRadius:
+                BorderRadius.circular(3), // Ajusta la curvatura de las esquinas
+          ),
+        ),
+        child: FittedBox(
+          // Asegura que el texto se ajuste al espacio disponible
+          child: Text(
+            widget.label,
+            style: const TextStyle(
+                fontSize: 10, color: Colors.black), // Puedes ajustar el tamaño del texto aquí
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 // Función para generar botones y posicionarlos.
 Widget generatePositionedButtons({
@@ -12,35 +67,29 @@ Widget generatePositionedButtons({
   required double y,
 }) {
   List<Widget> buttons = List.generate(count, (index) {
-    return Container(
-      height: 20,
-      width: 20,
-      margin: const EdgeInsets.all(2),
-      child: ElevatedButton(
-        onPressed: ()
-        {
-        
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromARGB(185, 9, 152, 255),
-          padding: EdgeInsets.zero, // Reduce el padding a cero
-          shape: RoundedRectangleBorder( // Asegura que el botón sea cuadrado
-            borderRadius: BorderRadius.circular(3), // Ajusta la curvatura de las esquinas
-          ),
-        ),
-        child: const FittedBox( // Asegura que el texto se ajuste al espacio disponible
-          child: Text(
-            " ",
-            style:  TextStyle(fontSize: 12), // Puedes ajustar el tamaño del texto aquí
-          ),
-        ),
-      ),
-    );
+    return ButtonWithColorChange(label: '$startingLabel${globalCounter++}');
   });
 
   return Positioned(
     left: x,
     top: y,
-    child: direction == ButtonDirection.horizontal ? Row(children: buttons) : Column(children: buttons),
+    child: direction == ButtonDirection.horizontal
+        ? Row(children: buttons)
+        : Column(children: buttons),
   );
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const PaginaPuestos(),
+    );
+  }
 }
--- prueba/lib/pages/pagina_puestos copy.dart
+++ prueba/lib/pages/pagina_puestos copy.dart
@@ -21,9 +21,9 @@ Widget contenedorPuestos() {
     body: Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       width: 450, // Ancho fijo para el contenedor
-      decoration:  const BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: Color.fromRGBO(168, 169, 171, 0.2)),
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Color.fromRGBO(168, 169, 171, 0.2)),
       child: SizedBox(
         height: 500, // Altura fija para el contenedor
         child: SingleChildScrollView(
@@ -35,98 +35,173 @@ Widget contenedorPuestos() {
               SizedBox(
                 // Asegúrate de que este alto sea suficiente para cubrir el espacio que necesitan tus botones
                 // Este alto se ajusta para demostrar el desplazamiento; ajusta según la necesidad
-                height: 800, // Ajusta esta altura basada en el contenido interno
+                height:
+                    800, // Ajusta esta altura basada en el contenido interno
                 child: Stack(
                   children: <Widget>[
-                    
-              generatePositionedButtons( count: 10, direction: ButtonDirection.vertical, startingLabel: "", x: 10, y: 10,),
-                
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 10,),
-              
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 50,),
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 75,),
-              
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 115,),
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 140,),
-              
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 180,),
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 205,),
-              
-              generatePositionedButtons( count: 10, direction: ButtonDirection.horizontal, startingLabel: "", x: 80, y: 245,),
-              
-              //pasillo
-              generatePositionedButtons( count: 25, direction: ButtonDirection.vertical, startingLabel: "", x: 395, y: 10,),
-              generatePositionedButtons( count: 10, direction: ButtonDirection.vertical, startingLabel: "", x: 330, y: 275,),
-                
-              generatePositionedButtons( count: 6, direction: ButtonDirection.horizontal, startingLabel: "", x: 180, y: 530,),
-                
-              generatePositionedButtons( count: 3, direction: ButtonDirection.horizontal, startingLabel: "", x: 250, y: 600,),
-              
-              generatePositionedButtons( count: 3, direction: ButtonDirection.vertical, startingLabel: "", x: 330, y: 630,),
-              
-              generatePositionedButtons( count: 4, direction: ButtonDirection.vertical, startingLabel: "", x: 220, y: 630,),
-                
-              generatePositionedButtons( count: 4, direction: ButtonDirection.vertical, startingLabel: "", x: 140, y: 630,),
-                
-              generatePositionedButtons( count: 5, direction: ButtonDirection.vertical, startingLabel: "", x: 80, y: 630,),
-                
-              generatePositionedButtons( count: 8, direction: ButtonDirection.vertical, startingLabel: "", x: 10, y: 560,),
-      
-              //Edificio CNC
-              Positioned(
-                left: 40,
-                top:300,
-                child: Container(
-                  height: 200,
-                  width: 280,  
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(168, 169, 171, 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(10))
-                    ),
-                  child: const Center(
-                    child: Text("CNC",style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,),
-                    ),
-                  ),
-                ),
-              ),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "A",
+                        x: 10,
+                        y: 10),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 10),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 50),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 80),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 120),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 150),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 190),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 220),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "A",
+                        x: 80,
+                        y: 255),
+                    generatePositionedButtons(
+                        count: 25,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "P",
+                        x: 395,
+                        y: 10),
+                    generatePositionedButtons(
+                        count: 10,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "P",
+                        x: 340,
+                        y: 295),
+                    generatePositionedButtons(
+                        count: 6,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "E",
+                        x: 150,
+                        y: 528),
+                    generatePositionedButtons(
+                        count: 3,
+                        direction: ButtonDirection.horizontal,
+                        startingLabel: "E",
+                        x: 245,
+                        y: 610),
+                    generatePositionedButtons(
+                        count: 3,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "E",
+                        x: 330,
+                        y: 645),
+                    generatePositionedButtons(
+                        count: 4,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "E",
+                        x: 220,
+                        y: 645),
+                    generatePositionedButtons(
+                        count: 4,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "E",
+                        x: 140,
+                        y: 645),
+                    generatePositionedButtons(
+                        count: 5,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "E",
+                        x: 80,
+                        y: 645),
+                    generatePositionedButtons(
+                        count: 8,
+                        direction: ButtonDirection.vertical,
+                        startingLabel: "E",
+                        x: 10,
+                        y: 560),
 
-              //Entrada secundario
-              Positioned(
-                left: 325,
-                top:0,
-                child: Container(
-                  height: 20,
-                  width: 65,  
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(168, 169, 171, 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(5))
+                    //Edificio CNC
+                    Positioned(
+                      left: 40,
+                      top: 300,
+                      child: Container(
+                        height: 200,
+                        width: 295,
+                        decoration: const BoxDecoration(
+                            color: Color.fromRGBO(168, 169, 171, 1.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        child: const Center(
+                          child: Text("CNC",
+                              style: TextStyle(
+                                  fontSize: 40, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
                     ),
-                  child: const Center(
-                    child: Text("Entrada",style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold,),
-                    ),
-                  ),
-                ),
-              ),
 
-              //Entrada Principal
-              Positioned(
-                left: 325,
-                top: 770,
-                child: Container(
-                  height: 30,
-                  width: 100,  
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(168, 169, 171, 1.0),
-                    borderRadius: BorderRadius.all(Radius.circular(5))
+                    //Entrada secundaria
+                    Positioned(
+                      left: 350,
+                      top: 0,
+                      child: Container(
+                        height: 20,
+                        width: 65,
+                        decoration: const BoxDecoration(
+                            color: Color.fromRGBO(168, 169, 171, 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        child: const Center(
+                          child: Text("Entrada",
+                              style: TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
                     ),
-                  child: const Center(
-                    child: Text("Entrada",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),
+
+                    //Entrada principal
+                    Positioned(
+                      left: 325,
+                      top: 770,
+                      child: Container(
+                        height: 30,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                            color: Color.fromRGBO(168, 169, 171, 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        child: const Center(
+                          child: Text("Entrada",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-
-
                   ],
                 ),
               ),
@@ -137,5 +212,3 @@ Widget contenedorPuestos() {
     ),
   );
 }
-
-
