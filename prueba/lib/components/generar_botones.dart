7a59d558bf5496975afc9771869a34b3d2827cde
Gabot22 <106275142+GaelBote@users.noreply.github.com>
2024-05-15 15:29:11 -0700

    Mapa de ubicacion

--- prueba/lib/components/generar_botones.dart
+++ prueba/lib/components/generar_botones.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:prueba/pages/pagina_puestos%20copy.dart';
 
 // Define una enumeración para la dirección de los botones.
 enum ButtonDirection { horizontal, vertical }
 
-// Variable global para mantener el contador.
-int globalCounter = 1;
+// Variable global para mantener los lugares seleccionados.
+List<String> selectedLocations = [];
 
 class ButtonWithColorChange extends StatefulWidget {
   final String label;
+  final Function updateCallback;
 
-  const ButtonWithColorChange({Key? key, required this.label})
-      : super(key: key);
+  const ButtonWithColorChange({
+    Key? key,
+    required this.label,
+    required this.updateCallback,
+  }) : super(key: key);
 
   @override
   _ButtonWithColorChangeState createState() => _ButtonWithColorChangeState();
@@ -23,6 +26,12 @@ class _ButtonWithColorChangeState extends State<ButtonWithColorChange> {
   void _toggleColor() {
     setState(() {
       isPressed = !isPressed;
+      if (isPressed) {
+        selectedLocations.add(widget.label);
+      } else {
+        selectedLocations.remove(widget.label);
+      }
+      widget.updateCallback();
     });
   }
 
@@ -37,20 +46,16 @@ class _ButtonWithColorChangeState extends State<ButtonWithColorChange> {
         style: ElevatedButton.styleFrom(
           backgroundColor: isPressed
               ? const Color.fromARGB(184, 255, 11, 11)
-              : const Color.fromARGB(185, 9, 152, 255),
-          padding: EdgeInsets.zero, // Reduce el padding a cero
+              : const Color.fromARGB(184, 1, 167, 62),
+          padding: EdgeInsets.zero,
           shape: RoundedRectangleBorder(
-            // Asegura que el botón sea cuadrado
-            borderRadius:
-                BorderRadius.circular(3), // Ajusta la curvatura de las esquinas
+            borderRadius: BorderRadius.circular(3),
           ),
         ),
         child: FittedBox(
-          // Asegura que el texto se ajuste al espacio disponible
           child: Text(
             widget.label,
-            style: const TextStyle(
-                fontSize: 10, color: Colors.black), // Puedes ajustar el tamaño del texto aquí
+            style: const TextStyle(fontSize: 10, color: Colors.black),
           ),
         ),
       ),
@@ -60,14 +65,18 @@ class _ButtonWithColorChangeState extends State<ButtonWithColorChange> {
 
 // Función para generar botones y posicionarlos.
 Widget generatePositionedButtons({
-  required int count,
+  required List<String> labels,
   required ButtonDirection direction,
-  required String startingLabel,
   required double x,
   required double y,
+  required Function updateCallback,
 }) {
-  List<Widget> buttons = List.generate(count, (index) {
-    return ButtonWithColorChange(label: '$startingLabel${globalCounter++}');
+  List<Widget> buttons = List.generate(labels.length, (index) {
+    final label = labels[index];
+    return ButtonWithColorChange(
+      label: label,
+      updateCallback: updateCallback,
+    );
   });
 
   return Positioned(
@@ -77,19 +86,4 @@ Widget generatePositionedButtons({
         ? Row(children: buttons)
         : Column(children: buttons),
   );
-}
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const PaginaPuestos(),
-    );
-  }
 }
